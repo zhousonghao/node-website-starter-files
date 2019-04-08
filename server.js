bbb70ebf32b2657be7ce479bb9a189037355a3f8
songhao zhou <songhaozhou@songhaos-MacBook-Pro-2.local>
2019-04-07 21:12:34 -0400

    fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+// const server = app.listen(7000, () => {
+//   console.log(`Express running → PORT ${server.address().port}`);
+// });
 
 app.get('/', (req, res) => {
   //res.render('index');
